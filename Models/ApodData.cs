cf3b85ebd4fbc264f54321cfbb7b8d61e8fcc346
Lisa <kareevaliza@gmail.com>
2026-02-12 17:13:09 +0000

    added a model to structure my api data

---
+++ Models/ApodData.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+
+namespace StargazingApp.Models;
+
+public class ApodData
+{
+    [JsonProperty("title")]
+    public string Title { get; set; } = string.Empty;
+
+    [JsonProperty("url")]
+    public string Url { get; set; } = string.Empty;
+
+    [JsonProperty("explanation")]
+    public string Explanation { get; set; } = string.Empty;
+
+    [JsonProperty("date")]
+    public string Date { get; set; } = string.Empty;
+
+    [JsonProperty("media_type")]
+    public string MediaType { get; set; } = string.Empty;
+
+    [JsonProperty("copyright")]
+    public string Copyright { get; set; } = string.Empty;
+}
