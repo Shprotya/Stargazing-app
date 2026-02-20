3d39e59890ac0c484f77f54bd4fa5739f7ba53e4
Lisa <kareevaliza@gmail.com>
2026-02-20 14:56:10 +0000

    added a contellation model

---
+++ Models/Constellation.cs
@@ -0,0 +1,16 @@
+﻿using SQLite;
+
+namespace StargazingApp.Models;
+
+public class Constellation
+{
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+
+    [Indexed] // Makes searching by name much faster
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+    public string BestVisibleMonth { get; set; }
+    public bool IsFavorite { get; set; }
+}
