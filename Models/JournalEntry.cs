77727d38017ba345f1a6414fc323cc98180e3b0f
Lisa <kareevaliza@gmail.com>
2026-03-30 20:14:45 +0100

    added a model for journal

---
+++ Models/JournalEntry.cs
@@ -0,0 +1,13 @@
+﻿using SQLite;
+
+namespace StargazingApp.Models;
+
+public class JournalEntry 
+{
+    public string Title { get; set; } = string.Empty;
+    public string Body { get; set; } = string.Empty;
+    public string Conditions { get; set; } = string.Empty;
+    public int Rating { get; set; } = 3;
+
+    
+}
